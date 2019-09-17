63a8e00dc122d993ee2820c6b24f875afb45a4cd
Travis CI <>
2019-09-17 12:57:13 +0000

    Deploy 6cd815e from branch develop

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -835,7 +835,7 @@ var indicatorDataStore = function(dataUrl) {
   }());
 
   var headlineColor = '777777';
-  if (String(this.indicatorId).contains('indicator')){
+  if (this.indicatorId.contains('ind')){
     var colors = ['e5243b', 'b71c2f', 'ea4f62', '891523', 'ef7b89', '5b0e17', 'f4a7b0', '2d070b', 'fce9eb'];
   }
   else if(this.indicatorId.contains('_3.')){
