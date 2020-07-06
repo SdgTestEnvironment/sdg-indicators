8b4f354ef01afe2097ed9c61091f3a4593203b02
Travis CI <>
2020-07-06 13:06:03 +0000

    Deploy 5014ede from branch develop

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -1576,7 +1576,7 @@ function prepareData(rows) {
       // object, for easier control: opensdg.dataRounding()
       if (typeof opensdg.dataRounding === 'function') {
         if (typeof opensdg.dataRoundingDp === 'function' & item.dcmplc){
-
+          item.Value = opensdg.dataRoundingDp(item.Value);
         }
         else{
           item.Value = opensdg.dataRounding(item.Value);
