948be5f580853fefb3b8918ce80494f1861d8636
MoBosse <50657629+MoBosse@users.noreply.github.com>
2020-12-10 15:26:35 +0000

    Deploying to gh-pages from  @ 783dee5af84d95cfd778bfcab9538e9578c0f7fc 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -1019,6 +1019,26 @@ opensdg.chartColors = function(indicatorId) {
   return this.colors;
 
 };
+opensdg.maptitles = function(indicatorId) {
+  if(indicatorId == "indicator_5-5-1"){
+
+    this.mapTitle = translations.t("seats held by women in national parliament")
+    this.mapUnit = translations.t("%")
+  }
+  else if(indicatorId == "indicator_8-5-1"){
+
+    this.mapTitle = translations.t("gender pay gap")
+    this.mapUnit = translations.t("%")
+  }
+  else if(indicatorId == "indicator_9-5-1"){
+
+    this.mapTitle = translations.t("proportion of r&d expenditures to gdp")
+    this.mapUnit = translations.t("%")
+  }
+
+  return [this.mapTitle, this.mapUnit] ;
+
+};
 var indicatorModel = function (options) {
 
   var helpers = 
