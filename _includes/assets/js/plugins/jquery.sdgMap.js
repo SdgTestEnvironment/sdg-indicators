9f53882a12c460f19e91e27d11cf7ce5f8350c4a
MoBosse <moritz.bosse@destatis.de>
2020-12-10 14:40:13 +0100

    use goal colrs for maps 2

--- _includes/assets/js/plugins/jquery.sdgMap.js
+++ _includes/assets/js/plugins/jquery.sdgMap.js
@@ -384,8 +384,6 @@
           .classes(plugin.options.colorRange[parseInt(goal)-1].length);
         //#1 map color depending on goal --- stop  ---
 
-          .domain(plugin.valueRange)
-          .classes(plugin.options.colorRange.length);
         plugin.years = _.uniq(availableYears).sort();
         plugin.currentYear = plugin.years[0];
 
