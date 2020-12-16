166463771eb9daad5dd27b437008440b6f6255df
MoBosse <MoBosse@users.noreply.github.com>
2020-12-16 01:14:19 +0000

    Deploying to gh-pages from  @ ea5e3a9608a206656d299909880938ed7a382ea1 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -2088,6 +2088,7 @@ function sortData(rows, selectedUnit) {
     dataHasGeoCodes: dataHasGeoCodes,
     dataHasSerieses: dataHasSerieses,
     getFirstUnitInData: getFirstUnitInData,
+    getFirstSeriesInData: getFirstSeriesInData,
     getDataByUnit: getDataByUnit,
     getDataBySeries: getDataBySeries,
     getDataBySelectedFields: getDataBySelectedFields,
