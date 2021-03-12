c18a7fb65ffcc8fa792eacf1f1a2d37e303d7fac
MoBosse <50657629+MoBosse@users.noreply.github.com>
2021-03-12 08:34:32 +0000

    Deploying to gh-pages from  @ 78e3bb6e69a4f4340e9af38a7aba0cadc356c153 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -2378,7 +2378,7 @@ function sortData(rows, selectedUnit) {
     }
 
     var combinations = helpers.getCombinationData(this.selectedFields);
-    //console.log("Input getDataset: ", headline, filteredData, combinations, this.years, translations.data.total, this.colors, this.selectableFields);
+    console.log("Input getDataset: ", headline, filteredData, combinations, this.years, translations.data.total, this.colors, this.selectableFields, this.showLine);
     var datasets = helpers.getDatasets(headline, filteredData, combinations, this.years, translations.data.total, this.colors, this.selectableFields, this.showLine);
     var selectionsTable = helpers.tableDataFromDatasets(datasets, this.years);
 
@@ -2804,7 +2804,7 @@ var indicatorView = function (model, options) {
     var that = this;
     var gridColor = that.getGridColor();
     var tickColor = that.getTickColor();
-    console.log("CahrtInfo: ", chartInfo);
+    //console.log("CahrtInfo: ", chartInfo);
     var chartConfig = {
       type: this._model.graphType,
       data: chartInfo,
