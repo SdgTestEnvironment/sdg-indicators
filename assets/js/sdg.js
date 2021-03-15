f79fc8fed996ad31cb94470629474708db320901
MoBosse <50657629+MoBosse@users.noreply.github.com>
2021-03-15 13:35:17 +0000

    Deploying to gh-pages from  @ 5004ac7bcf1d7fc2b76791f5a2e6aa0e06b1a5da 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -2056,7 +2056,7 @@ function getOpenColorInfo(colorAssignments, colors) {
       for (var colorIndex = 0; colorIndex < colors.length; colorIndex++) {
         if (!(assignedColors.includes(colorIndex))) {
           return {
-            colorIndex: colorIndex,
+            colorIndex: colorIndex + 1,
             striped: stripedState,
           }
         }
