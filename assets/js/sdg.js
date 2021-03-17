8f627ca5f74ff52d208cbe5ec92c0c6c8fc7e792
MoBosse <50657629+MoBosse@users.noreply.github.com>
2021-03-17 06:37:14 +0000

    Deploying to gh-pages from  @ 5b962be6627e2c42acdd20f781f01fdb626615ee 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -3341,7 +3341,7 @@ var indicatorView = function (model, options) {
     else{
       var goal = indicator[10,11]
     }
-    console.log(' site.graph_color_headline[' + goal +'] ', goal-1  );
+    console.log('', goal-1  );
     return this.isHighContrast(contrast) ? '#FFDD00' : '#00006a';
   };
 
