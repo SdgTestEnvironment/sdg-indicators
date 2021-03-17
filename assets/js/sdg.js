3e8a4c1f0ce08509d792ac627d3e0e11fa117ad1
MoBosse <50657629+MoBosse@users.noreply.github.com>
2021-03-17 08:01:25 +0000

    Deploying to gh-pages from  @ 7cf3df671b1644b6f0982e7d433dae37cd1df61d 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -3327,7 +3327,7 @@ var indicatorView = function (model, options) {
   };
 
   this.getHeadlineColor = function(contrast) {
-    return this.isHighContrast(contrast) ? '#FFDD00' : '#e5243b#dda63a#4c9f38#c5192d#ff3a21#26bde2#fcc30b#a21942#fd6925#dd1367#fd9d24#bf8b2e#3f7e44#0a97d9#56c02b#00689d#19486a';
+    return this.isHighContrast(contrast) ? '#FFDD00' : '#00006a';
   }
 
   this.getGridColor = function(contrast) {
