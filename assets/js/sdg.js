61ced00bc69ec8570484e0691d2a0fcb98213bf7
MoBosse <50657629+MoBosse@users.noreply.github.com>
2021-03-18 14:20:51 +0000

    Deploying to gh-pages from  @ 78dccfe96b157c4379d40f37248c87d4064217bc 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -2146,6 +2146,8 @@ function makeDataset(years, rows, combination, labelFallback, color, background,
     borderWidth: 2,
     data: prepareDataForDataset(years, rows),
     excess: excess,
+    spanGaps: spanGaps,
+    showLine: showLine,
   });
 }
 
