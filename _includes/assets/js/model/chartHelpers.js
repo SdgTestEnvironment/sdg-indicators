7642f7855ddaed842298cb00f469783af01ab044
MoBosse <moritz.bosse@destatis.de>
2021-03-18 07:22:04 +0100

    no headline Color 1

--- _includes/assets/js/model/chartHelpers.js
+++ _includes/assets/js/model/chartHelpers.js
@@ -49,9 +49,17 @@ function getDatasets(headline, data, combinations, years, defaultLabel, colors, 
   var datasets = [], index = 0, dataset, colorIndex, color, background, border, striped, excess, combinationKey, colorAssignment, showLine, spanGaps;
   var numColors = colors.length,
       maxColorAssignments = numColors * 2;
+  var offSet = 0;
 
   prepareColorAssignments(colorAssignments, maxColorAssignments);
   setAllColorAssignmentsReadyForEviction(colorAssignments);
+
+  if (headline.length > 0) {
+    dataset = makeHeadlineDataset(years, headline, defaultLabel, colors, showLine, spanGaps);
+    datasets.unshift(dataset);
+    var offSet = 1;
+  }
+
 
   combinations.forEach(function(combination) {
     var filteredData = getDataMatchingCombination(data, combination, selectableFields);
@@ -82,7 +90,7 @@ function getDatasets(headline, data, combinations, years, defaultLabel, colors, 
         }
       }
 
-      color = getColor(colorIndex, colors);
+      color = getColor(colorIndex, colors, offSet);
       background = getBackground(color, striped);
       border = getBorderDash(striped);
 
@@ -95,10 +103,10 @@ function getDatasets(headline, data, combinations, years, defaultLabel, colors, 
   //datasets.sort(function(a, b) { return (a.label > b.label) ? 1 : -1; });
 
 
-  if (headline.length > 0) {
-    dataset = makeHeadlineDataset(years, headline, defaultLabel, showLine, spanGaps);
-    datasets.unshift(dataset);
-  }
+  // if (headline.length > 0) {
+  //   dataset = makeHeadlineDataset(years, headline, defaultLabel, colors, showLine, spanGaps);
+  //   datasets.unshift(dataset);
+  // }
   return datasets;
 }
 
@@ -237,8 +245,8 @@ function assignColor(colorAssignment, combination, colorIndex, striped) {
  * @param {Array} colors
  * @return Color from a list
  */
-function getColor(colorIndex, colors) {
-  return '#' + colors[colorIndex];
+function getColor(colorIndex, colors, offSet) {
+  return '#' + colors[colorIndex + offSet];
 }
 
 /**
@@ -355,14 +363,14 @@ function getHeadlineColor() {
  * @param {string} label
  * @return {Object} Dataset object for Chart.js
  */
-function makeHeadlineDataset(years, rows, label, showLine, spanGaps) {
+function makeHeadlineDataset(years, rows, label, colors, showLine, spanGaps) {
   var dataset = getBaseDataset();
   return Object.assign(dataset, {
     label: label,
-    borderColor: getHeadlineColor(),
-    backgroundColor: getHeadlineColor(),
-    pointBorderColor: getHeadlineColor(),
-    pointBackgroundColor: getHeadlineColor(),
+    borderColor: colors[0], //getHeadlineColor(),
+    backgroundColor: colors[0], //getHeadlineColor(),
+    pointBorderColor: colors[0], //getHeadlineColor(),
+    pointBackgroundColor: colors[0], //getHeadlineColor(),
     borderWidth: 4,
     data: prepareDataForDataset(years, rows),
     showLine: showLine,
