d841f911ee3bb629005e664602a8dd026b55792d
MoBosse <50657629+MoBosse@users.noreply.github.com>
2021-04-16 05:28:38 +0000

    Deploying to gh-pages from  @ d9a555cd4370ada7a3818eb16fc0171c1db3dc93 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -3757,6 +3757,11 @@ var indicatorView = function (model, options) {
     .appendTo(fieldGroupElement.find('#indicatorData .variable-options'));
   }
 };
+// @deprecated start
+// Some backwards compatibiliy code after Lodash migration.
+_.findWhere = _.find;
+// @deprecated end
+
 var indicatorController = function (model, view) {
   this._model = model;
   this._view = view;
