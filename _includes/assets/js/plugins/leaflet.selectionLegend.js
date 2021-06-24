c602c3510348c6de27e23690d8e9176f52f17a54
MoBosse <moritz.bosse@destatis.de>
2021-06-24 14:02:18 +0200

    log

--- _includes/assets/js/plugins/leaflet.selectionLegend.js
+++ _includes/assets/js/plugins/leaflet.selectionLegend.js
@@ -86,10 +86,12 @@
         '</li>';
       var plugin = this.plugin;
       var valueRange = this.plugin.valueRange;
+
+      console.log("VR: ", valueRange);
       if (valueRange[0] =='NaN'){
         valueRange.shift()
       }
-      
+
       selectionList.innerHTML = this.selections.map(function(selection) {
         var value = plugin.getData(selection.feature.properties);
         var percentage, valueStatus;
