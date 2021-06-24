090498a928ab6f1ebd3d6ba7663985c040f4f20d
MoBosse <moritz.bosse@destatis.de>
2021-06-24 12:41:51 +0200

    fixes for map 2.4.1

--- _includes/assets/js/mapTitles.js
+++ _includes/assets/js/mapTitles.js
@@ -1,5 +1,10 @@
 opensdg.maptitles = function(indicatorId) {
-  if(indicatorId == "indicator_5-5-1"){
+  if(indicatorId == "indicator_2-4-1"){
+
+    this.mapTitle = translations.t("organically farmed agricultural land (data from destatis)")
+    this.mapUnit = translations.t("%")
+  }
+  else if(indicatorId == "indicator_5-5-1"){
 
     this.mapTitle = translations.t("seats held by women in national parliament")
     this.mapUnit = translations.t("%")
