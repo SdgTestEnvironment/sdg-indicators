db257913ac0d04800d7a608e8e792f0dc5c1f01c
MoBosse <50657629+MoBosse@users.noreply.github.com>
2021-07-12 06:07:13 +0000

    Deploying to gh-pages from  @ 940f826736be7872afec5c98f02318c8c9989556 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -3322,6 +3322,7 @@ var indicatorView = function (model, options) {
               callback: function(value) {
                 return view_obj.alterDataDisplay(value, undefined, 'chart y-axis tick');
               },
+              stepWidth: 2,
             },
             scaleLabel: {
               display: this._model.selectedUnit ? translations.t(this._model.selectedUnit) : this._model.measurementUnit,
