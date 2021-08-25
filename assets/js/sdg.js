e322569f8291451bf74609f873a6e270c64d846b
MoBosse <50657629+MoBosse@users.noreply.github.com>
2021-08-25 10:39:45 +0000

    Deploying to gh-pages from  @ 45756fc206ccbbdd47d8766da0cf299df92062f0 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -3180,9 +3180,6 @@ var indicatorView = function (model, options) {
     if (units.length < 1) {
     
       $(this._rootElement).addClass('no-units');
-      console.log("1");
-    } else {
-      console.log("2");
     }
   };
 
