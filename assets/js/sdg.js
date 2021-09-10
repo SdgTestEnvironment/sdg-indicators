cac7160e9c8ffc698db52e84dc5fbfa382aef542
MoBosse <50657629+MoBosse@users.noreply.github.com>
2021-09-10 08:38:50 +0000

    Deploying to gh-pages from  @ 57deb8c8b7b18495ef2c9eb464b2e6445b0f776b 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -3433,6 +3433,7 @@ var indicatorView = function (model, options) {
           callbacks: {
             label: function(tooltipItems, data) {
               var label = data.datasets[tooltipItems.datasetIndex].label
+              label = label.replace('<sub>','').replace('</sub>','');
               if (label.length > 45){
 
                 label = label.split(' ');
@@ -3451,6 +3452,7 @@ var indicatorView = function (model, options) {
             afterLabel: function(tooltipItems, data) {
 
               var label = data.datasets[tooltipItems.datasetIndex].label;
+              label = label.replace('<sub>','').replace('</sub>','');
               if (label.length > 45){
                 label = label.split(' ');
                 var re = [];
