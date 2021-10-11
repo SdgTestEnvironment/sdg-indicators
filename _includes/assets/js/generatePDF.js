38f4df439c72ba936af8f51afa05bbd86b798907
MoBosse <moritz.bosse@destatis.de>
2021-10-11 13:48:21 +0200

    test download 4

--- _includes/assets/js/generatePDF.js
+++ _includes/assets/js/generatePDF.js
@@ -1,6 +1,8 @@
 generatePDF = function () {
   // Choose the element that our invoice is rendered in.
-  const element = getElementById('headerBanner');
+  const element = document.getElementById('headerBanner');
+  //<div class="container">
+  //document.getElementById('chart-canvas'),
   // Choose the element and save the PDF for our user.
   html2pdf().from(element).save();
 }
