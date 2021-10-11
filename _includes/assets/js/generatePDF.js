f97b1b48641dd27f3f000a8f6ca85173a90e9604
MoBosse <moritz.bosse@destatis.de>
2021-10-11 14:55:38 +0200

    test download 8

--- _includes/assets/js/generatePDF.js
+++
@@ -1,8 +0,0 @@
-var generatePDF = function () {
-  // Choose the element that our invoice is rendered in.
-  const element = document.getElementById('indicatorTitle');
-  //<div class="container">
-  //document.getElementById('chart-canvas'),
-  // Choose the element and save the PDF for our user.
-  html2pdf().from(element).save();
-}
