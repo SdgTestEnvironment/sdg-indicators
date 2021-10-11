83b4d88ae569736d6bb07c477c3fa87707116cf5
MoBosse <50657629+MoBosse@users.noreply.github.com>
2021-10-11 12:26:03 +0000

    Deploying to gh-pages from  @ d275c1e97cab1db2c5bb2437a0dc9a0c5fc2ead1 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -2951,7 +2951,7 @@ var mapView = function () {
 };
 var generatePDF = function () {
   // Choose the element that our invoice is rendered in.
-  const element = document.getElementById('headerBanner');
+  const element = document.getElementById('indicatorTitle');
   //<div class="container">
   //document.getElementById('chart-canvas'),
   // Choose the element and save the PDF for our user.
