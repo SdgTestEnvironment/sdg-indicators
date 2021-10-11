da3ef9eeec3239551259ec52e044946ede988fff
MoBosse <50657629+MoBosse@users.noreply.github.com>
2021-10-11 13:44:02 +0000

    Deploying to gh-pages from  @ f70225d007bd60d58d397c3cee8853d0eeab817d 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -2949,14 +2949,6 @@ var mapView = function () {
     });
   };
 };
-var generatePDF = function () {
-  // Choose the element that our invoice is rendered in.
-  const element = document.getElementById('indicatorTitle');
-  //<div class="container">
-  //document.getElementById('chart-canvas'),
-  // Choose the element and save the PDF for our user.
-  html2pdf().from(element).save();
-}
 //Last check: 17.09.2021
 var indicatorView = function (model, options) {
 
