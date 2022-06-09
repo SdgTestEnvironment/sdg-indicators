da06fa087836d5b71f7194e4842659bb2ab89ec8
MoBosse <moritz.bosse@destatis.de>
2022-06-09 08:20:25 +0200

    yearSlider 2

--- _includes/assets/js/plugins/jquery.sdgMap.js
+++ _includes/assets/js/plugins/jquery.sdgMap.js
@@ -431,7 +431,7 @@
         //#1 map color depending on goal --- stop  ---
 
         plugin.years = _.uniq(availableYears).sort();
-        plugin.currentYear = plugin.years[0];
+        plugin.currentYear = plugin.years.slice(-1)[0];
 
         // And we can now update the colors.
         plugin.updateColors();
