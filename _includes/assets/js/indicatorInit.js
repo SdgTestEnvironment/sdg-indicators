14bcb5241cb6e72f8c16c6f6f4e307db4b1a1694
MoBosse <moritz.bosse@destatis.de>
2022-06-15 14:05:42 +0200

    1.4.2

--- _includes/assets/js/indicatorInit.js
+++ _includes/assets/js/indicatorInit.js
@@ -43,6 +43,7 @@ var indicatorInit = function () {
                         graphAnnotations: domData.graphannotations,
                         graphTargetLines: domData.graphtargetlines,
                         graphSeriesBreaks: domData.graphseriesbreaks,
+                        graphErrorBars: domData.grapherrorbars,
                         indicatorDownloads: domData.indicatordownloads,
                         dataSchema: domData.dataschema,
                         compositeBreakdownLabel: domData.compositebreakdownlabel,
