bc9509bf0d36bd977dde1be6caef8a56b674122c
MoBosse <50657629+MoBosse@users.noreply.github.com>
2022-06-15 11:41:52 +0000

    Deploying to gh-pages from  @ 2a325f767049d2c2b3e27bbeaa517d26720247e9 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -3044,7 +3044,7 @@ function getTimeSeriesAttributes(rows) {
       selectedSeries: this.selectedSeries,
       graphLimits: helpers.getGraphLimits(this.graphLimits, this.selectedUnit, this.selectedSeries),
       stackedDisaggregation: this.stackedDisaggregation,
-      graphAnnotations: helpers.getGraphAnnotations(this.graphAnnotations, this.selectedUnit, this.selectedSeries, this.graphTargetLines, this.graphSeriesBreaks, , this.graphErrorBars),
+      graphAnnotations: helpers.getGraphAnnotations(this.graphAnnotations, this.selectedUnit, this.selectedSeries, this.graphTargetLines, this.graphSeriesBreaks, this.graphErrorBars),
       chartTitle: this.chartTitle,
       chartType: this.graphType,
       indicatorDownloads: this.indicatorDownloads,
