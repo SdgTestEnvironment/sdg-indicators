8245a7bcf43246f112e4c8dd3cb1f50f20e80617
MoBosse <moritz.bosse@destatis.de>
2022-06-20 09:59:42 +0200

    refresh

--- _includes/components/charts/annotation_presets.js
+++ _includes/components/charts/annotation_presets.js
@@ -78,12 +78,5 @@ opensdg.annotationPresets = {
         backgroundColor: '#757575',
         xScaleID: 'x',
         yScaleID: 'y',
-        arrowHeads: {
-            display: true,
-            enabled: true,
-            fill: true,
-            length: 80,
-            width: 80,
-          },
     },
 };
