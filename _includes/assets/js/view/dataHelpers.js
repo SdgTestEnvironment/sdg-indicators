5399c630f5ec704f0cf1bdda3714f25eb4d5a589
MoBosse <moritz.bosse@destatis.de>
2022-09-07 15:17:35 +0200

    fix rounding function

--- _includes/assets/js/view/dataHelpers.js
+++ _includes/assets/js/view/dataHelpers.js
@@ -42,7 +42,8 @@ function alterDataDisplay(value, info, context) {
       var precision = VIEW._precision
     }
     if (precision || precision === 0) {
-        altered = Number.parseFloat(altered).toFixed(precision);
+        //altered = Number.parseFloat(altered).toFixed(precision);
+        altered = Math.round(altered*10**precision)/10**precision
     }
     // Now apply our custom decimal separator if needed.
     if (OPTIONS.decimalSeparator) {
