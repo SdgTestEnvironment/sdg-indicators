7e4275260fb43bac89c3bf71b383f88c613aaf9a
MoBosse <50657629+MoBosse@users.noreply.github.com>
2022-10-12 11:48:11 +0000

    Deploying to gh-pages from  @ de03c3b12017f924df6b55e19d8f8ac5cbfd58da 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -4457,8 +4457,13 @@ function alterDataDisplay(value, info, context) {
     // Before passing to user-defined dataDisplayAlterations, let's
     // do our best to ensure that it starts out as a number.
     var altered = value;
+    // In case the decimal separator has already been applied,
+    // change it back now.
+    if (typeof altered === 'string' && OPTIONS.decimalSeparator) {
+        altered = altered.replace(OPTIONS.decimalSeparator, '.');
+    }
     if (typeof altered !== 'number') {
-        altered = Number(value);
+        altered = Number(altered);
     }
     // If that gave us a non-number, return original.
     if (isNaN(altered)) {
@@ -4473,11 +4478,6 @@ function alterDataDisplay(value, info, context) {
     // Special treatment for numbers on y axis: If stepSize is defined, they should display decimal places as follows:
     // StepSize >= 1 --> 0 decimal places, Stepsize >= 0.1 --> 1 decimal place, StepSize >= 0.01 --> 2 decimal places ...
     if (context == 'chart y-axis tick' && VIEW._graphStepsize && VIEW.graphStepsize != 0 && VIEW.graphStepsize != '') {
-      console.log("StepSize: ", VIEW._graphStepsize.step);
-      console.log("1/StepSize: ", (1 / VIEW._graphStepsize.step));
-      console.log("Log: ", Math.log(1 / VIEW._graphStepsize.step));
-      console.log("Log/Ln: ", Math.log(1 / VIEW._graphStepsize.step) / Math.LN10);
-      console.log("Round: ", Math.ceil(Math.log(1 / VIEW._graphStepsize.step) / Math.LN10));
       precision = Math.ceil(Math.log(1 / VIEW._graphStepsize.step) / Math.LN10);
       if (precision < 0) {
         precision = 0
@@ -4487,9 +4487,7 @@ function alterDataDisplay(value, info, context) {
       var precision = VIEW._precision
     }
     if (precision || precision === 0) {
-        //altered = Number.parseFloat(altered).toFixed(precision);
-        altered = Math.round(altered*10**precision)/10**precision
-        altered = altered.toFixed(precision)
+        altered = Number.parseFloat(altered).toFixed(precision);
     }
     // Now apply our custom decimal separator if needed.
     if (OPTIONS.decimalSeparator) {
