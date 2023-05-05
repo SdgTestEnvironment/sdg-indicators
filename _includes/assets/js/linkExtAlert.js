e386580b7aef71157bd1a88ffffca04483bedc54
Moritz Bosse <moritz.bosse@destatis.de>
2023-05-05 16:36:49 +0200

    shot inthe dark towards external link alert

---
+++ _includes/assets/js/linkExtAlert.js
@@ -0,0 +1,3 @@
+function confirm_alert(node) {
+    return confirm( "Sie verlassen unsere Webseite! Bitte beachten Sie, dass der folgende Link eine Webseite öffnet, für deren Inhalt wir nicht verantwortlich sind und auf die unsere Datenschutzbestimmung keine Anwendung findet." );
+}
