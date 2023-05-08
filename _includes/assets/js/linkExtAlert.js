efcf0fdb057d12b602a7b75477f5b2b3b5cbcab5
Moritz Bosse <moritz.bosse@destatis.de>
2023-05-08 10:00:32 +0200

    2.1

--- _includes/assets/js/linkExtAlert.js
+++ _includes/assets/js/linkExtAlert.js
@@ -1,9 +1,8 @@
 function confirm_alert(source) {
-  if (source && source != ' {
+  if (source && source != {
     var text = 'Sie verlassen unsere Webseite!\nDer Link führt Sie zur Webseite '
     return confirm(text + source + '.');
-  }
-  else{
+  } else {
     return confirm('Sie verlassen unsere Webseite!\nDer Link führt Sie zu einer externen Webseite.')
   }
 }
