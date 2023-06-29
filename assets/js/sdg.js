ace0ec71b05bc375234f78d6d61690ffd0cf47dd
Moritz Bosse <moritz.bosse@destatis.de>
2023-06-29 16:10:29 +0200

    deleting -info files

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -1,32 +1,27 @@
----
-# Don't delete this line.
----
-{%- if site.progressive_web_app.enabled -%}
-{%- include assets/js/serviceWorkerInit.js -%}
-{%- endif -%}
-{%- include assets/js/autotrack-element.js -%}
-{%- include assets/js/plugins/jquery.sdgMap.js -%}
-{%- include assets/js/chartjs/rescaler.js -%}
-{%- include assets/js/chartjs/noDataMessage.js -%}
-{%- include assets/js/chartjs/accessibleCharts.js -%}
-{%- include assets/js/event.js -%}
-{%- include assets/js/accessibility.js -%}
-{%- include assets/js/chartColors.js -%}
-{%- include assets/js/indicatorModel.js -%}
-{%- include assets/js/mapView.js -%}
-{%- include assets/js/indicatorView.js -%}
-{%- include assets/js/indicatorController.js -%}
-{%- include assets/js/indicatorInit.js -%}
-{%- include assets/js/tabs.js -%}
-{%- include assets/js/accessibleTabs.js -%}
-{%- include assets/js/search.js -%}
-{%- include assets/js/cookieConsent.js -%}
-{%- include assets/js/lib/classList.js -%}
-{%- include assets/js/lib/modernizr-custom.js -%}
-{%- include assets/js/plugins/leaflet.selectionLegend.js -%}
-{%- include assets/js/plugins/leaflet.yearSlider.js -%}
-{%- include assets/js/plugins/leaflet.fullscreenAccessible.js -%}
-{%- include assets/js/plugins/leaflet.searchAccessible.js -%}
-{%- include assets/js/plugins/leaflet.disaggregationControls.js -%}
-{%- include assets/js/backToTop.js -%}
-{%- include assets/js/linkExtAlert.js -%}
+// Here you can override any of the Sass variables from Open SDG. This is the
+// easiest way to change things like colors and dimensions. Below are a few
+// examples of variables. For a full list, see this folder:
+// https://github.com/open-sdg/open-sdg/tree/master/_sass/variables
+
+//$goal-colors: white white white white white white white white white white white white white white white white white;
+$goal-banner-backgroundColors: #E52B3D #DDA63A #4C9F38 #C42130 #FF3E24 #28BCE1 #FCC30B #A21942 #FD6925 #DD1367 #FD9C27 #BF8B2E #417D46 #0A96D8 #57BE2F #00689C #1F4A6A;
+$goal-banner-color: white;
+
+$status-backgroundColor-complete: #5cb85c;
+$status-backgroundColor-inprogress: #f0ad4e;
+$status-backgroundColor-notstarted: #E27874;
+$status-backgroundColor-notapplicable: #B7B7B7;
+$status-backgroundColor-inner-notapplicable: #B7B7B7;
+
+
+$status-color-complete: #111111;
+$status-color-inprogress: #111111;
+$status-color-notstarted: #111111;
+$status-color-notapplicable: #111111;
+
+$status-border-complete: none;
+$status-border-inprogress: none;
+$status-border-notstarted: none;
+$status-border-notapplicable: none;
+
+$menu-dropdownLink-backgroundColor-hover: #f5f5f5 !default;
