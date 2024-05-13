cdc29bd5fb5b49c5acd95c9451bb8c1166b5decc
MoBosse <50657629+MoBosse@users.noreply.github.com>
2024-05-13 05:52:27 +0000

    Deploying to gh-pages from  @ 973eba6703563a86b9a061f0ec5de4ae5acbc0db 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -4844,6 +4844,7 @@ function alterDataDisplay(value, info, context, additionalInfo) {
         obsAttributes = obsAttributesTable.data[row][col];
     }
     if (obsAttributes.length > 0) {
+        console.log('obsAttributes: ', obsAttributes);
         var obsAttributeFootnoteNumbers = obsAttributes.map(function(obsAttribute) {
             return getObservationAttributeFootnoteSymbol(obsAttribute.footnoteNumber);
         });
