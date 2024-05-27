2a25cb396d3046ef742c8c641a060b396ea7c7b3
Moritz Bosse <moritz.bosse@destatis.de>
2024-05-27 09:30:55 +0200

    0.0

--- _includes/assets/js/view/fieldHelpers.js
+++ _includes/assets/js/view/fieldHelpers.js
@@ -123,7 +123,7 @@ function getObservationAttributeText(obsAttribute) {
     }
     var label = translations.t(obsAttribute.value);
     if (attributeConfig.label) {
-        label = label + ': ' + translations.t(label);
+        label = translations.t(attributeConfig.label) + ': ' + label;
     }
     return label;
 }
