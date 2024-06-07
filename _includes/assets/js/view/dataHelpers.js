b6deaa62b524357c53812c8a8733a09ea2f094d6
Moritz Bosse <moritz.bosse@destatis.de>
2024-06-07 15:14:31 +0200

    2.0

--- _includes/assets/js/view/dataHelpers.js
+++ _includes/assets/js/view/dataHelpers.js
@@ -68,7 +68,7 @@ function alterDataDisplay(value, info, context, additionalInfo) {
     }
     if (obsAttributes.length > 0) {
         var obsAttributeFootnoteNumbers = obsAttributes.map(function(obsAttribute) {
-            return getObservationAttributeFootnoteSymbol(obsAttribute.footnoteNumber);
+            return getObservationAttributeFootnoteSymbol(obsAttribute);
         });
         altered += ' ' + obsAttributeFootnoteNumbers.join(' ');
     }
@@ -81,6 +81,7 @@ function alterDataDisplay(value, info, context, additionalInfo) {
  * @param {int} num
  * @returns {string} Number converted into unicode character for footnotes.
  */
-function getObservationAttributeFootnoteSymbol(num) {
-    return '[' + translations.indicator.note + ' ' + (num + 1) + ']';
+function getObservationAttributeFootnoteSymbol(obsAttribute) {
+    return obsAttribute.value;
+    //return '[' + translations.indicator.note + ' ' + (num + 1) + ']';
 }
--- _includes/assets/js/view/fieldHelpers.js
+++ _includes/assets/js/view/fieldHelpers.js
@@ -105,7 +105,8 @@ function updateObservationAttributes(obsAttributes) {
     Object.values(obsAttributes).forEach(function(obsAttribute) {
         var label = getObservationAttributeText(obsAttribute),
             num = getObservationAttributeFootnoteSymbol(obsAttribute.footnoteNumber);
-        var $listItem = $('<dt id="observation-footnote-title-' + num + '">' + num + '</dt><dd id="observation-footnote-desc-' + num + '">' + label + '</dd>');
+        //var $listItem = $('<dt id="observation-footnote-title-' + num + '">' + num + '</dt><dd id="observation-footnote-desc-' + num + '">' + label + '</dd>');
+        var $listItem = $('<dl><dt><u>' + translations.t('symbols') + ':</u></dt></dl>');
         $listElement.append($listItem);
         if (label.includes(',')) {
           var single_labels = label.split(',');
