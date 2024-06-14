e1719252b4e7f0c6b0d9f8574c381be9def06982
MoBosse <50657629+MoBosse@users.noreply.github.com>
2024-06-14 11:58:49 +0000

    Deploying to gh-pages from  @ 2176de4f2167c3c75b55f840ee1f80f98b024d49 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -3489,6 +3489,7 @@ function updateObservationAttributes(obsAttributes) {
     }
     $listElement.show();
     Object.values(obsAttributes).forEach(function(obsAttribute) {
+        var test = $listElement.toString();
         var label = getObservationAttributeText(obsAttribute),
             num = obsAttribute.footnoteNumber;//getObservationAttributeFootnoteSymbol(obsAttribute.footnoteNumber);
         //var $listItem = $('<dt id="observation-footnote-title-' + num + '">' + num + '</dt><dd id="observation-footnote-desc-' + num + '">' + label + '</dd>');
@@ -3505,7 +3506,7 @@ function updateObservationAttributes(obsAttributes) {
               var x = ''};
             console.log("4: ",$listElement);
             var $listItem = $('<dd id="observation-footnote-desc-' + num + '">' + single_labels[i] + ': ' +  translations.t('+++' + single_labels[i]) + '</dd>');
-            if (!$listElement.includes($listItem)){
+            if (!test.includes($listItem)){
               $listElement.append($listItem);
             };
             console.log("5: ",$listElement);
@@ -3513,7 +3514,7 @@ function updateObservationAttributes(obsAttributes) {
         }
         else
           var $listItem = $('<dd id="observation-footnote-desc-' + num + '">' + obsAttribute.value + ': ' + translations.t('+++' + label) + '</dd>');
-          if (!$listElement.includes($listItem)){
+          if (!test.includes($listItem)){
             $listElement.append($listItem);
           var x = '';
           }
