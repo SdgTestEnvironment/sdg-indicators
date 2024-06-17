cde971ee32af35acc2a4e25395e06bed7caa8b7f
MoBosse <50657629+MoBosse@users.noreply.github.com>
2024-06-17 14:05:09 +0000

    Deploying to gh-pages from  @ a15cd9e69ba9408ed3e4719cab20337ed1d3c3f3 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -4685,6 +4685,7 @@ function createTable(table, indicatorId, el, isProxy, observationAttributesTable
 
         table.data.forEach(function (data) {
             var row_html = '<tr>';
+            console.log("XX", data);
             table.headings.forEach(function (heading, index) {
                 // For accessibility set the Year column to a "row" scope th.
                 var isYear = (index == 0);
@@ -4692,7 +4693,7 @@ function createTable(table, indicatorId, el, isProxy, observationAttributesTable
                 var cell_suffix = (isYear) ? '</th>' : '</td>';
                 //var cell_content = (isYear) ? translations.t(data[index]) : data[index];
                 //row_html += cell_prefix + (isYear ? '' : ' class="table-value"') + '>' + (cell_content !== null &&  cell_content !== undefined ?  cell_content : '.') + cell_suffix;
-                row_html += cell_prefix + (isYear ? '' : ' class="table-value"') + '>' + (data[index] !== null &&  data[index] !== undefined ?  data[index] : '.') + cell_suffix;
+                row_html += cell_prefix + (isYear ? '' : ' class="table-value"') + '>' + (data[index] !== null && data[index] !== undefined ?  data[index] : '.') + cell_suffix;
             });
             row_html += '</tr>';
             currentTable.find('tbody').append(row_html);
