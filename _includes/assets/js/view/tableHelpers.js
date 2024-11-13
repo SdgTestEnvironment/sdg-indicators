30d6f5d3b268165949a12efea177eab29d519e33
Moritz Bosse <moritz.bosse@destatis.de>
2024-11-13 15:30:43 +0100

    4.2.8

--- _includes/assets/js/view/tableHelpers.js
+++ _includes/assets/js/view/tableHelpers.js
@@ -250,11 +250,12 @@ function createTable(table, indicatorId, el, isProxy, observationAttributesTable
                 //var cell_content = (isYear) ? translations.t(data[index]) : data[index];
                 //row_html += cell_prefix + (isYear ? '' : ' class="table-value"') + '>' + (cell_content !== null &&  cell_content !== undefined ?  cell_content : '.') + cell_suffix;
                 //row_html += cell_prefix + (isYear ? '' : ' class="table-value"') + '>' + (data[index] !== null && data[index] !== undefined ?  (data[index] + ' ' + obsValue) : obsValue) + cell_suffix;
-                row_html += cell_prefix + (isYear ? '' : ' class="table-value"') + '>' + (data[index] !== null && data[index] !== undefined ?  dateForTable : obsValue) + cell_suffix;
+                row_html += cell_prefix + (isYear ? '' : ' class="table-value"') + '>' + (data[index] !== null && data[index] !== undefined ?  (dateForTable + "X") : (obsValue + "Y")) + cell_suffix;
                 console.log("ROW FIN", row_html);
             });
             row_html += '</tr>';
             currentTable.find('tbody').append(row_html);
+            console.log("currentTable", currentTable);
         });
 
         $(el).append(currentTable);
