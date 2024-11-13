e7a281e708d4004854fcc97b9d07ae7548ae18a9
Moritz Bosse <moritz.bosse@destatis.de>
2024-11-13 08:40:17 +0100

    1.1

--- _includes/assets/js/view/tableHelpers.js
+++ _includes/assets/js/view/tableHelpers.js
@@ -208,7 +208,7 @@ function createTable(table, indicatorId, el, isProxy, observationAttributesTable
                 var isYear = (index == 0);
                 var cell_prefix = (isYear) ? '<th scope="row"' : '<td';
                 var cell_suffix = (isYear) ? '</th>' : '</td>';
-                var dateForTable = data[index] == 0 ? ("" + obsValue.replace('[','').replace(']','')) : (data[index] + ' ' + obsValue)
+                var dateForTable = (data[index] == 0 || obsValue == '0') ? (obsValue.replace('[','').replace(']','')) : (data[index] + ' ' + obsValue)
                 //var cell_content = (isYear) ? translations.t(data[index]) : data[index];
                 //row_html += cell_prefix + (isYear ? '' : ' class="table-value"') + '>' + (cell_content !== null &&  cell_content !== undefined ?  cell_content : '.') + cell_suffix;
                 //row_html += cell_prefix + (isYear ? '' : ' class="table-value"') + '>' + (data[index] !== null && data[index] !== undefined ?  (data[index] + ' ' + obsValue) : obsValue) + cell_suffix;
