4680296f76af888c7329aa5d365708d3cafaaeba
Moritz Bosse <moritz.bosse@destatis.de>
2024-11-25 10:11:44 +0100

    1.1

--- _includes/assets/js/view/dataHelpers.js
+++ _includes/assets/js/view/dataHelpers.js
@@ -129,6 +129,13 @@ function alterDataDisplay(value, info, context, additionalInfo) {
  * @returns {string} Number converted into unicode character for footnotes.
  */
 function getObservationAttributeFootnoteSymbol(obsAttribute) {
-    return '' + obsAttribute.value + '';
+    // make sure we do not get 0.000 for obsValue
+    if (isNaN(parseInt(obsAttribute.value))) {
+        return '' + obsAttribute.value + '';
+    }
+    else{
+        return '' + parseInt(obsAttribute.value) + '';
+    }
+
     //return '[' + translations.indicator.note + ' ' + (num + 1) + ']';
 }
