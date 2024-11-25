fabd6397deefc957ef6077bd18c616dcb89d1688
MoBosse <50657629+MoBosse@users.noreply.github.com>
2024-11-25 09:59:47 +0000

    Deploying to gh-pages from  @ 52ebf16bff03989759c7f7ad45dd18b1f5009d8c 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -5013,7 +5013,7 @@ function getObservationAttributeFootnoteSymbol(obsAttribute) {
         return '' + obsAttribute.value + '';
     }
     else{
-        return '' + string(parseInt(obsAttribute.value)) + '';
+        return '' + String(parseInt(obsAttribute.value)) + '';
     }
 
     //return '[' + translations.indicator.note + ' ' + (num + 1) + ']';
