8ebef07ed676480e29a52bc446b22ab39a3307ae
MoBosse <50657629+MoBosse@users.noreply.github.com>
2024-11-29 12:00:07 +0000

    Deploying to gh-pages from  @ 7ffc7bddabde10ef145b1fd04ab61e3b40b823eb 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -3068,6 +3068,7 @@ function getAllObservationAttributes(rows) {
     this.selectedSeries = this.serieses[0];
     if (this.hasStartValues) {
       this.selectedSeries = helpers.getSeriesFromStartValues(this.startValues) || this.selectedSeries;
+      console.log("We have startValues:", this.selectedSeries);
     }
     this.refreshSeries();
   }
