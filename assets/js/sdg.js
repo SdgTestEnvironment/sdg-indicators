bedd96835fec73a3a466b1f80a01f69141cc3895
MoBosse <50657629+MoBosse@users.noreply.github.com>
2024-12-02 12:09:21 +0000

    Deploying to gh-pages from  @ 51543e8ae32e76fc7df31be27c2a42a2cc18f8c9 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -1613,7 +1613,7 @@ function getSeriesFromStartValues(startValues) {
   var match = startValues.find(function(startValue) {
     return startValue.field === SERIES_COLUMN;
   }, this);
-  console.log('match, sries column:', match, SERRIES_COLUMN);
+  console.log('match, sries column:', match, SERIES_COLUMN);
   return (match) ? match.value : false;
 }
 
