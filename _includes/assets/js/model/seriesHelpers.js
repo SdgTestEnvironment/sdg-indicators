f3109507e475565d9e32ac203751272a768f986a
Moritz Bosse <moritz.bosse@destatis.de>
2024-12-02 12:42:58 +0100

    1.2

--- _includes/assets/js/model/seriesHelpers.js
+++ _includes/assets/js/model/seriesHelpers.js
@@ -77,6 +77,6 @@ function getSeriesFromStartValues(startValues) {
   var match = startValues.find(function(startValue) {
     return startValue.field === SERIES_COLUMN;
   }, this);
-  console.log('SeriesStart:', match);
+  console.log('match, sries column:', match, SERRIES_COLUMN);
   return (match) ? match.value : false;
 }
